fb9caab53bf7265e08ca0c599564e3fe106cd8d0
ferror18 <ferror18@gmail.com>
2021-01-22 11:12:44 -0800

    placeholder utilities for hours validation

---
+++ hours/utility.js
@@ -0,0 +1,15 @@
+module.exports.isValidForPost = async function(params) {
+    return true
+}
+
+module.exports.isValidForDelete = async function(params) {
+    return true
+}
+
+module.exports.isValidForPatch = async function(params) {
+    return true
+}
+
+module.exports.isValidForGet = async function(params) {
+    return true
+}
